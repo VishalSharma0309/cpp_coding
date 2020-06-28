913adb6d2aba9f2bfc39faf51554fbc11d6496bb
VishalSharma0309 <vishals7654@gmail.com>
2020-06-29 03:30:23 +0530

    adding BST

---
+++ trees/BSTUse.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include "BinaryTreeNode.h"
+
+using namespace std;
+
+/* 
+Binary Search Tree
+- A type of Binary Tree inspired from Binary Search (O(logn))
+- suppose you're searching for 's'. We start the search from root 'r'
+    - if r==s -> found
+    - else if r>s -> seach in right sub-tree
+    - else search in left sub-tree
+
+- Properties of BST:
+    1. For every node 'n': 
+        everyting left to n is smaller than n->data
+        everything right to n is greater than n->data
+*/
+
+
+int main (){
+
+    return 0;
+}
--- trees/BinaryTreeLeetCodePractice.cpp
+++ trees/BinaryTreeLeetCodePractice.cpp
@@ -72,10 +72,6 @@ BinaryTreeNode <int> * takeInputLevelWise () {
 }
 
 
-
-
-
-
 /*
 563. Binary Tree Tilt
 The tilt of a tree node is defined as the absolute difference between the 
@@ -156,13 +152,125 @@ BinaryTreeNode <int> * invertTree(BinaryTreeNode <int> * root) {
     return root;    
 }
 
+/*
+110. Balanced Binary Tree
+a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+*/
 
+int height (BinaryTreeNode <int> * root){
+    if (root == NULL){
+        return 0;
+    }
+    return 1 + max (height (root->left) , height (root->right)); 
+}
+
+bool balancedTree (BinaryTreeNode <int> * root){
+    if (root==NULL){
+        return true;
+    }
+    cout << abs (height(root->left) - height (root->right)) << endl;
+    if (abs (height(root->left) - height (root->right)) > 1){
+        cout << abs (height(root->left) - height (root->left)) << endl;
+        return false;
+    }
+    return (balancedTree (root->left) && balancedTree (root->right));
+}
+
+/*
+572. Subtree of Another Tree
+
+*/
+
+bool isSame (BinaryTreeNode <int> * p , BinaryTreeNode <int> * q){
+    if (p==NULL || q==NULL){
+        if (p==NULL && q==NULL)
+            return true;
+        return false;
+    }
+
+    if (p->data != q->data){
+        return false;
+    }
+
+    return (isSame (p->left , q->left) && isSame (p->right , q->right));
+     
+}
+
+bool isSubtree (BinaryTreeNode <int> * p , BinaryTreeNode <int> * q){
+    if (isSame (p,q)){
+        return true;
+    }
+    bool ans = false;
+    if (p!=NULL){
+        ans = (isSubtree (p->left , q) || isSubtree (p->right , q));
+    }
+    return ans;
+}
+
+
+/*
+872. Leaf-Similar Trees
+Two binary trees are considered leaf-similar if their leaf value sequence is the same.
+*/
+
+vector <int> ansLeaf;
+vector <int> leafNodes (BinaryTreeNode <int> * root){
+    if (root!=NULL){    
+        if (root->left == NULL && root->right == NULL){
+            ansLeaf.push_back(root->data);
+        }
+        leafNodes (root->left);
+        leafNodes (root->right);
+    }
+    return ansLeaf;
+}
+
+bool leafSimilar (BinaryTreeNode <int> * root1 , BinaryTreeNode <int> * root2){
+    vector <int> leaf1 = leafNodes (root1);
+    ansLeaf.clear();
+    vector <int> leaf2 = leafNodes (root2);
+    ansLeaf.clear();
+    for (int i=0 ; i<leaf1.size() ; ++i){
+        cout << leaf1[i] << " ";
+    }
+    cout << endl;
+    for (int i=0 ; i<leaf2.size() ; ++i){
+        cout << leaf2[i] << " ";
+    }
+    cout << endl;
+    return (leaf1 == leaf2);
+}
+
+/*
+111. Minimum Depth of Binary Tree
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+*/
+
+int minDepth (BinaryTreeNode <int> * root){
+    
+}
+
+// 1 2 2 3 3 -1 -1 4 4 -1 -1 -1 -1 -1 -1
+
+/*
+3 5 1 6 2 9 8 -1 -1 7 4 -1 -1 -1 -1 -1 -1 -1 -1
+3 5 1 6 7 4 2 -1 -1 -1 -1 -1 -1 9 8 -1 -1 -1 -1
+*/
 
 int main(){
 
-    BinaryTreeNode <int> * root = takeInputLevelWise ();
-    printTreeLevelWise (root);
+    BinaryTreeNode <int> * root1 = takeInputLevelWise ();
+    printTreeLevelWise (root1);
 
-    cout << findTiltRec (root);
+    
+    BinaryTreeNode <int> * root2 = takeInputLevelWise ();
+    printTreeLevelWise (root2);
+
+    //cout << findTiltRec (root);
+    //cout << height (root) << endl;
+    //cout << balancedTree (root) << endl; 
+    
+    cout << leafSimilar (root1 , root2) << endl;
+
     return 0;
 }
